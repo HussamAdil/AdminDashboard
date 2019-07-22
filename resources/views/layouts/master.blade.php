824b15733cef2de7fea4cbd0b9ef8b033cba6f70
HussamAdil <hussam0638@gmail.com>
2019-07-22 19:44:54 +0200

    chage active links

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -179,7 +179,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
           </li>
         
         <li class="nav-item has-treeview menu-open">
-          <a href="#" class="nav-link active">
+          <a href="#" class="nav-link  ">
             <i class="nav-icon fa fa-cog"></i>
             <p>
               Managment
